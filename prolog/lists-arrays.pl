43c37f31bfa2010f89311952c3b90ad4e484f286
Dimitar Kalinkov <dkalinkov93@gmail.com>
2016-04-09 19:05:22 +0300

    Added transpose matrix function and renamed file.

--- prolog/lists-arrays.pl
+++ prolog/lists-arrays.pl
@@ -6,3 +6,12 @@ apnd([H|A], X, [H|Y]):- apnd(A,X,Y).
 rev(A, X):- rev(A, X, []).
 rev([H|A], B, X):- rev(A, B, [H|X]).
 rev([], X, X). 
+
+% Тransposing a matrix
+transpose([[]|_], []).
+transpose(M, [X|T]):- 
+    row(M, X, M1), 
+    transpose(M1, T).
+
+row([], [], []).
+row([[X|Xs]|Ys], [X|R], [Xs|Z]):- row(Ys, R, Z).
